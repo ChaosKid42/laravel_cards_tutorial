4ee5dded2549ac898571ae800c06a2f3003b19e8
ChaosKid42 <christoph.scholz@gmail.com>
2016-05-01 16:13:08 +0200

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Document</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+        @yield('header')
+
+    </head>
+    <body>
+
+        <div class="container">
+            @yield('content')
+        </div>
+
+    </body>
+</html>
