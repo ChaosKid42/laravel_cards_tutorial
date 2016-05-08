cf8417b5df753a98022369f59d80abacceba9bb6
ChaosKid42 <christoph.scholz@gmail.com>
2016-05-08 16:15:51 +0200

    included browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -14,13 +14,16 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix.sass('app.scss')
     .copy('bower_components/bootstrap-sass/assets/fonts', 'public/build/fonts')
-    .copy('bower_components/font-awesome/fonts/', 'public/build/fonts')
-    .copy('bower_components/lato/font/', 'public/build/font')
+    .copy('bower_components/font-awesome/fonts', 'public/build/fonts')
+    .copy('bower_components/lato/font', 'public/build/font')
 	.scripts([
 		'../../../bower_components/jquery/dist/jquery.js',
 		'../../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js'
 	], 'public/js/app.js')
-    .version([ 'css/app.css', 'js/app.js' ]);
+    .version([ 'css/app.css', 'js/app.js' ])
+    .browserSync({
+        proxy: 'localhost:8000'
+    });
 });
 
 
