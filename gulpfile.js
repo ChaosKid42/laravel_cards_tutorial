ff223939940b00ac444bd957e755ff2ba7a8b1d4
ChaosKid42 <christoph.scholz@gmail.com>
2016-05-14 12:26:12 +0200

    removed bower

--- gulpfile.js
+++ gulpfile.js
@@ -13,14 +13,11 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.sass('app.scss')
-    .copy('bower_components/bootstrap-sass/assets/fonts', 'public/build/fonts')
-    .copy('bower_components/font-awesome/fonts', 'public/build/fonts')
-    .copy('bower_components/lato/font', 'public/build/font')
+    .copy('node_modules/bootstrap-sass/assets/fonts', 'public/build/fonts')
+    .copy('node_modules/font-awesome/fonts', 'public/build/fonts')
+    .copy('node_modules/lato-font/fonts', 'public/build/fonts')
 	.copy('vendor/caouecs/laravel-lang/src/de', 'resources/lang/de')
-	.scripts([
-		'../../../bower_components/jquery/dist/jquery.js',
-		'../../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js'
-	], 'public/js/app.js')
+    .browserify('app.js')
     .version([ 'css/app.css', 'js/app.js' ])
     .browserSync({
         proxy: 'localhost:8000'
